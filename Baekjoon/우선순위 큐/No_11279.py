767da0a6d1964ddc0fd8260cde7939ce8ebe892a
gnwjd309 <fujeong15@naver.com>
2022-05-03 16:11:37 +0000

    Solve 11279 again

--- Baekjoon/우선순위 큐/No_11279.py
+++ Baekjoon/우선순위 큐/No_11279.py
@@ -1,14 +1,17 @@
-import sys
 import heapq
+import sys
 
-heap = []
-n = int(sys.stdin.readline())
-for _ in range(n):
-  m = int(sys.stdin.readline())
-  if m == 0:
-    if len(heap) == 0:
-      print(0)
+N = int(sys.stdin.readline())
+heap = list()
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x==0:
+        if not heap:
+            print(0)
+        else:
+            print(abs(heapq.heappop(heap)))
+    
     else:
-      print((-1)*heapq.heappop(heap))
-  else:
-    heapq.heappush(heap, (-1)*m)
+        heapq.heappush(heap, -x)
